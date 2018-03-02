eaaa13505a4dd9529189959b0f645b4d2001265e
Guillaume Collet <bilouweb@free.fr>
2018-03-02 23:19:11 +0100

    added an api route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'React5min' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'React5min 2' });
 });
 
+/* Creates test for JSON */
+const testJSON = [
+  {
+    name: 'John Smith',
+    username: 'CaptainCode'
+  },
+  {
+    name: 'Jane Doe',
+    username: 'JaneyCakes'
+  }
+];
+
+/* Send JSON if requested */
+router.get('/sendjson', (req, res, next) => {
+  res.json(testJSON)
+});
 module.exports = router;
