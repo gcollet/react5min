a6ed0cf83704cd2c48a365b8bec9e2504963d070
Guillaume Collet <bilouweb@free.fr>
2018-03-02 23:28:19 +0100

    added mongoose and user model

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const index = require('./routes/index');
 const api = require('./routes/api/index');
 
 const app = express();
+
+// Connect Mongoose
+mongoose.connect('mongodb://localhost/musiclist');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
