8627914dd53bd05f2298e4c83fc1a3bd4f674cb0
Guillaume Collet <bilouweb@free.fr>
2018-03-24 21:35:56 +0100

    added react component

--- src/testcomponent.jsx
+++ src/testcomponent.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-export default function TestComponent() {
+export default function TestComponent(props) {
+  const { headline, count, showCount } = props;
   return (
     <div>
-      <h1>React Test Component</h1>
+      <h1>React Test Component jolafrite</h1>
+      <h2>{headline}</h2>
+      { showCount ? <p>{count}</p> : null }
     </div>
   );
 }
